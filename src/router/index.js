05f0b81e365d2febd3b18b4e78956ba8e67f87a0
joe <joe.ulica@gmail.com>
2022-08-31 12:58:42 -0400

    add show cart index

--- src/router/index.js
+++ src/router/index.js
@@ -54,6 +54,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Productsedit.vue')
+  },
+  {
+    path: '/cart',
+    name: 'showcart',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Showcart.vue')
   }
 
 ]
---
+++ src/views/Showcart.vue
@@ -0,0 +1,42 @@
+<script>
+import axios from 'axios'
+export default {
+  data: function () {
+    return {
+      message: "Here is your cart",
+      products: []
+    };
+  },
+  created: function () {
+    this.getData()
+  },
+  methods: {
+    getData: function () {
+      console.log(`getting data`)
+      axios.get("/carted_products.json").then(response => {
+        this.products = response.data
+      })
+    }
+  },
+};
+</script>
+  
+  <template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <div v-for="product in products">
+      Product: {{ product.product }} &nbsp;&nbsp;
+      price: {{ product.price }} &nbsp;&nbsp;
+      quantity: {{ product.quantity }}
+      <br />
+    </div>
+    <p v-if="!products[0]">You have nothing in your cart</p>
+  </div>
+</template>
+  
+  <style>
+  #image {
+    height: 200px;
+    width: 200px;
+  }
+  </style>
